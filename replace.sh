ef8d569b179c7be8c3d70633c2001a9bb7d85d00
Asuma Yamada <asuma@linear-jp.biz>
2021-04-18 22:41:06 +0900

    fix: fix indent and format

--- replace.sh
+++ replace.sh
@@ -4,10 +4,9 @@
 # misspell <file path> | ./replace.sh
 
 
-while read LINE
-do
- filepath=`echo $LINE | cut -d":" -f1`
- wrongword=`echo $LINE | cut -d'"' -f2`
- correctword=`echo $LINE | cut -d'"' -f4`
- sed -i "" "s/$wrongword/$correctword/g" $filepath
+while read line; do
+  filepath=`echo $line | cut -d ":" -f1`
+  wrongword=`echo $line | cut -d '"' -f2`
+  correctword=`echo $line | cut -d '"' -f4`
+  sed -i "" "s/$wrongword/$correctword/g" $filepath
 done
